c4ff78f181181d9a0c8c9746e8432dd5477b4702
hushino <frostshock@outlook.com>
2020-12-11 09:43:56 -0300

    f

--- insert.php
+++ insert.php
@@ -1,6 +1,8 @@
 <?php
 require "conexion.php";
-require './php-image-resize-master/lib/ImageResize.php';
+//require './php-image-resize-master/lib/ImageResize.php';
+require_once 'function.resize.php';
+require_once 'smart_resize_image.function.php';
 
 $titular = $_POST['titular'];
 $nacionalidad = $_POST['nacionalidad'];
@@ -19,12 +21,16 @@ $cat = $_POST['cat'];
 $target_path = "images/" . basename($_FILES["foto"]['name']);
 
 
-$image = new \Gumlet\ImageResize($target_path);
-$image->resizeToHeight(500);
-$image->save('image2.jpg');
-
 if (move_uploaded_file($_FILES["foto"]['tmp_name'], $target_path)) {
 
+    //$file = $target_path;
+
+    //indicate the path and name for the new resized file
+    //$resizedFile = $target_path;
+
+    //call the function (when passing path to pic)
+    //smart_resize_image($file, null,100, 230, false, $resizedFile, false, false, 40);
+      
 
     $sql = "INSERT INTO `filiacion`(`titular`, `nacionalidad`, `fechadenacimiento`, `domicilio`, `licencia`, `gruposanguinio`, `rh`, `disp`,`expediente`,`letra`,`anio`,`fecha`,`foto`,`cat`) 
         VALUES 
