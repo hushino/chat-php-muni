0c157893d975335e638fe302955dd5596b4fc474
hushino <frostshock@outlook.com>
2020-12-18 10:38:36 -0300

    f

--- chatticketsmuni/chat_ajax/index.php
+++ chatticketsmuni/chat_ajax/index.php
@@ -3,16 +3,17 @@ include "db.php";
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>CHAT CON PHP, MYSQL Y AJAX</title>
 	<link rel="stylesheet" type="text/css" href="estilos.css">
 	<link href="https://fonts.googleapis.com/css?family=Mukta+Vaani" rel="stylesheet">
 
 	<script type="text/javascript">
-		function ajax(){
+		function ajax() {
 			var req = new XMLHttpRequest();
 
-			req.onreadystatechange = function(){
+			req.onreadystatechange = function() {
 				if (req.readyState == 4 && req.status == 200) {
 					document.getElementById('chat').innerHTML = req.responseText;
 				}
@@ -23,9 +24,12 @@ include "db.php";
 		}
 
 		//linea que hace que se refreseque la pagina cada segundo
-		setInterval(function(){ajax();}, 1000);
+		setInterval(function() {
+			ajax();
+		}, 1000);
 	</script>
 </head>
+
 <body onload="ajax();">
 
 	<div id="contenedor">
@@ -34,29 +38,30 @@ include "db.php";
 		</div>
 
 		<form method="POST" action="index.php">
-			<input type="text" name="nombre" placeholder="Ingresa tu nombre">			
+			<input type="text" name="nombre" placeholder="Ingresa tu nombre">
 			<textarea name="mensaje" placeholder="Ingresa tu mensaje"></textarea>
 			<input type="submit" name="enviar" value="Enviar">
 		</form>
 
 		<?php
-			if (isset($_POST['enviar'])) {
-				
-				$nombre = $_POST['nombre'];
-				$mensaje = $_POST['mensaje'];
+		if (isset($_POST['enviar'])) {
+
+			$nombre = $_POST['nombre'];
+			$mensaje = $_POST['mensaje'];
 
 
-				$consulta = "INSERT INTO `chat` (nombre, mensaje) VALUES ('$nombre', '$mensaje')";
+			$consulta = "INSERT INTO `chat` (nombre, mensaje) VALUES ('$nombre', '$mensaje')";
 
-				$ejecutar = $conexion->query($consulta);
+			$ejecutar = $conexion->query($consulta);
 
-				if ($ejecutar) {
-					echo "<embed loop='false' src='beep.mp3' hidden='true' autoplay='true'>";
-				}
+			if ($ejecutar) {
+				echo "<embed loop='false' src='beep.mp3' hidden='true' autoplay='true'>";
 			}
+		}
 
 		?>
 	</div>
 
 </body>
+
 </html>
